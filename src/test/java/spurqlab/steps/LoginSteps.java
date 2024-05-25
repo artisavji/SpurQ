a5466c5d31afc1e3b5ba497fa936882877634b81
artisavji <savjiarti@gmail.com>
2024-05-25 12:58:10 +0530

    Framework Assessment

--- src/test/java/spurqlab/steps/LoginSteps.java
+++ src/test/java/spurqlab/steps/LoginSteps.java
@@ -1,44 +1,44 @@
-package spurqlab.steps;
+    package spurqlab.steps;
 
-import com.google.common.collect.Table;
-import spurqlab.Pages.LoginPage;
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.testng.Assert;
-import spurqlab.Pages.ProductPage;
-import spurqlab.core.TextContext;
+    import com.google.common.collect.Table;
+    import spurqlab.Pages.LoginPage;
+    import io.cucumber.java.en.And;
+    import io.cucumber.java.en.Given;
+    import io.cucumber.java.en.Then;
+    import io.cucumber.java.en.When;
+    import org.testng.Assert;
+    import spurqlab.Pages.ProductPage;
+    import spurqlab.core.TextContext;
 
-public class LoginSteps /*extends TextContext*/ {
-    LoginPage loginPage;
-    ProductPage productPage;
+    public class LoginSteps /*extends TextContext*/ {
+        LoginPage loginPage;
+        ProductPage productPage;
 
-    public LoginSteps() {
-        this.loginPage = new LoginPage();
-        this.productPage = new ProductPage();
-    }
+        public LoginSteps() {
+            this.loginPage = new LoginPage();
+            this.productPage = new ProductPage();
+        }
 
-    @Given("User is on SauceLabs login page")
-    public void userIsOnSauceLabsLoginPage() {
+        @Given("User is on SauceLabs login page")
+        public void userIsOnSauceLabsLoginPage() {
 
-    }
+        }
 
-    @When("User enters {string} and {string}")
-    public void userEntersAnd(String userName, String password) {
-        loginPage.enterCredentials(userName, password);
+        @When("User enters {string} and {string}")
+        public void userEntersAnd(String userName, String password) {
+            loginPage.enterCredentials(userName, password);
 
-    }
+        }
 
-    @And("User clicks on login button")
-    public void userClicksOnButton() {
-        loginPage.clickLogin();
-    }
+        @And("User clicks on login button")
+        public void userClicksOnButton() {
+            loginPage.clickLogin();
+        }
 
-    @Then("user is on product page with list of {int} products")
-    public void userIsOnProductPageWithListOfProducts(int expectedResult) {
-        Assert.assertEquals(productPage.getProductCount(), expectedResult);
-    }
+        @Then("user is on product page with list of {int} products")
+        public void userIsOnProductPageWithListOfProducts(int expectedResult) {
+            Assert.assertEquals(productPage.getProductCount(), expectedResult);
+        }
 
    /* @When("User enters credentials given below:")
     public void userEntersCredentialsGivenBelow(Table dataTable) {
